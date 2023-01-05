8f6ae70e504612a1227b848ccbb7815fa82afc68
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-05 13:00:14 +0100

    add deleting button

---
+++ src/components/buttons/Delete.tsx
@@ -0,0 +1,7 @@
+import btn from './buttons.module.css';
+type DeleteProps = {
+    handleClick: ()=> void;
+}
+export function Delete({ handleClick }: DeleteProps) {
+    return <div className={btn.delete} onClick={()=> handleClick}/>
+}
