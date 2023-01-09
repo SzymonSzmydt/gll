e60c63ff9a0641e2340fd0e11b38684bc69f163a
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-09 21:01:39 +0100

    fix data display

--- src/context/hooks/functions.tsx
+++ src/context/hooks/functions.tsx
@@ -1,2 +1,10 @@
+import { Db } from "../redux/dbSlice";
 export const arrayGenerator = (n: number) =>
   Array.from({ length: n }, (_, i) => i + 1);
+
+export const howManyOccured = (n: number, data: Db[]) =>
+  data
+    .map((e: any) =>
+      e["normal1"].includes(n) ? { num: n, date: e.date } : null
+    )
+    .filter((e: any) => e != null);
--- src/pages/dashboard/controlPanel/ControlPanel.tsx
+++ src/pages/dashboard/controlPanel/ControlPanel.tsx
@@ -1,17 +1,48 @@
+import ctr from "./control.module.css";
 import { WindowShadow } from "../../../components/windows/WindowShadow";
 import { Nav } from "./nav/Nav";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../context/redux/Store";
-import { arrayGenerator } from '../../../context/hooks/functions';
+import {
+  arrayGenerator,
+  howManyOccured,
+} from "../../../context/hooks/functions";
 
 export function ControlPanel() {
-  const db = useSelector((state: RootState) => state.database.value);
+  const db = Object.values(
+    useSelector((state: RootState) => state.database.value)
+  );
+
+  const data = db
+    ? arrayGenerator(50).map((e: number) => howManyOccured(e, db))
+    : [];
+  console.log(data);
+
   return (
     <WindowShadow>
       <Nav />
-      <div>
-
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <td className={ctr.td}> Liczba </td>
+            <td className={ctr.td}> Wystąpienia </td>
+            <td className={ctr.td}> Oscylacja </td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0
+            ? data.map((e, i) => (
+                <tr key={i} className={ctr.tr}>
+                  <td className={ctr.td}> {e[0]?.num} </td>
+                  <td className={ctr.td}> {e.length} </td>
+                  <td className={ctr.td}>
+                    {`${((e.length / db.length) * 100).toFixed()} %`}
+                  </td>
+                </tr>
+              ))
+            : null}
+        </tbody>
+      </table>
     </WindowShadow>
   );
 }
