45f9f513e6dca312d06be8660cd4922e2a23a72e
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-13 22:09:06 +0100

    add generata data map function

--- src/pages/dashboard/controlPanel/Generate.tsx
+++ src/pages/dashboard/controlPanel/Generate.tsx
@@ -1,13 +1,18 @@
 import { dataWithDraw } from "../../../context/redux/dbSlice";
+import { Ball } from "../../../components/buttons/Ball";
 type GenerateProps = {
   data50: dataWithDraw[];
 };
 
 export function Generate({ data50 }: GenerateProps) {
+  console.log(data50);
   return (
     <div>
       <section>
         <p> Liczby, które najrzadziej wypadły</p>
+        {data50.map((e) =>
+          e.draw[e.draw.length - 1] >= 15 ? <Ball>{e.num} </Ball> : null
+        )}
       </section>
     </div>
   );
