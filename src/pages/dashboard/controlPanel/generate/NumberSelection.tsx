51506891944a715f3ae0695ad6f95a50a7a3c40b
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-20 15:58:07 +0100

    add logic to component

---
+++ src/pages/dashboard/controlPanel/generate/NumberSelection.tsx
@@ -0,0 +1,64 @@
+import { Window } from "../../../../components/windows/Window";
+import { Ball } from "../../../../components/buttons/Ball";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../context/redux/Store";
+import { Dispatch, SetStateAction } from "react";
+import gen from "./gen.module.css";
+import { DataWithDraw } from "../../../../context/redux/dbSlice";
+
+type NumberSelectionProps = {
+  min: number;
+  max: number;
+  setData: Dispatch<SetStateAction<DataWithDraw>>;
+  setIsVisible: Dispatch<SetStateAction<boolean>>;
+  data50: DataWithDraw[];
+};
+
+export function NumberSelection({
+  min,
+  max,
+  setData,
+  setIsVisible,
+  data50,
+}: NumberSelectionProps) {
+  const keys = useSelector((state: RootState) => state.database.keys);
+  const data12 = useSelector((state: RootState) => state.database.data12);
+
+  const handleClick = (item: DataWithDraw) => {
+    setIsVisible(true);
+    setData(item);
+  };
+
+  return (
+    <section className={gen.positon}>
+      <Window>
+        <p>
+          Liczby, które wypadły od {min} do {max} losowań temu.
+        </p>
+        <span className={gen.small}> Losowanie standardowe 1 z 50 </span>
+        <div className={gen.black}>
+          {data50.map((e) =>
+            e.draw[e.draw.length - 1] > min &&
+            e.draw[e.draw.length - 1] < max ? (
+              <Ball key={e.num} handleClick={() => handleClick(e)}>
+                {e.num}
+              </Ball>
+            ) : null
+          )}
+        </div>
+        <span className={gen.small}> Losowanie dodatkowe 1 z 12 </span>
+        <div className={gen.black}>
+          {data12.map((e) =>
+            e.draw[e.draw.length - 1] > min &&
+            e.draw[e.draw.length - 1] < max &&
+            !e.date.includes(keys[keys.length - 1]) ? (
+              <Ball key={e.num} handleClick={() => handleClick(e)}>
+                {e.num}
+              </Ball>
+            ) : null
+          )}
+        </div>
+      </Window>
+    </section>
+  );
+}
