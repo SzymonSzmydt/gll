1bae084e1db80e111e1c6e95d1d5ef6061e71a95
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-24 13:21:18 +0100

    upgrade serarch, added countion info

--- src/components/ui/popup/SearchResults.tsx
+++ src/components/ui/popup/SearchResults.tsx
@@ -13,31 +13,38 @@ type SearchProsp = {
 
 export function SearchResults({ numbersToFind, setIsVisible }: SearchProsp) {
   const db = useSelector((state: RootState) => state.database.value);
-  const arrayNumbersToFind: number[] = numbersToFind.split(",").map((e) => +e);
+  const arrayNumbersToFind: number[] = numbersToFind.includes(",")
+    ? Array.from(new Set(numbersToFind.split(",").map((e) => +e)))
+    : Array.from(new Set(numbersToFind.split(" ").map((e) => +e)));
   const resultOfSearch = checkIfNumbersAreInDb(arrayNumbersToFind, db);
-
   console.log(resultOfSearch);
 
   return (
     <WindowWithCloseBtn handleClose={() => setIsVisible(false)}>
-      Lista wyników
-      <hr />
-      <BlackArea name={"Szukane Liczby"}>
-        {arrayNumbersToFind.map((e) => (
-          <Ball key={e}> {e} </Ball>
-        ))}
-      </BlackArea>
-      <section>
-        Wystąpienia
-        <hr />
-        {resultOfSearch.map((e) => (
-          <BlackArea name={e.date}>
-            {e.nums.map((num) => (
-              <Ball> {num} </Ball>
+      {resultOfSearch.length > 0 ? (
+        <>
+          Lista wyników
+          <hr />
+          <BlackArea name={"Szukane Liczby"}>
+            {arrayNumbersToFind.map((e) => (
+              <Ball key={e}> {e} </Ball>
             ))}
           </BlackArea>
-        ))}
-      </section>
+          <section>
+            Wystąpienia
+            <hr />
+            {resultOfSearch.map((e) => (
+              <BlackArea name={e.date}>
+                {e.nums.map((num) => (
+                  <Ball> {num} </Ball>
+                ))}
+              </BlackArea>
+            ))}
+          </section>
+        </>
+      ) : (
+        "Dane do przeszukania, muszą być liczbami, które są od siebie różne"
+      )}
     </WindowWithCloseBtn>
   );
 }
